8bd4b3d980189b7a84f005056824d955fc2cc2e4
Cleveland Williams <cameron3579@gmail.com>
2016-11-06 13:07:45 -0500

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,5 @@ Rails.application.routes.draw do
 	root 'users#index'
 	resources :users 
 	resources :sessions
-	resources :songs
-#	post '/sessions' => 'sessions#create'
-#	post '/users' => 'users#create'
-#  get '/songs' => 'songs#index'
-#	post '/songs' => 'songs#create'
-	delete '/sessions' => 'sessions#destroy'
-#  get 'songs/new'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
