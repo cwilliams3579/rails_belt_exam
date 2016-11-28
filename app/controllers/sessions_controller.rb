69894d80a0958df7c810a00b27bf3b640bfa1f4a
Cleveland Williams <clevelandwilliams3579@gmail.com>
2016-11-28 15:51:48 -0600

    removed duplicates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,31 +24,3 @@ class SessionsController < ApplicationController
 			params.require(:login).permit(:email, :password)
 	end
 end
-
-class SessionsController < ApplicationController
-	def new
-	end
-
-	def create
-		user = User.find_by(email: login_params[:email])
-		if user && user.authenticate(login_params[:password])
-			session[:user_id] = user.id
-			redirect_to "/songs"
-		else
-			flash[:login_error] = "Invalid please try again"
-			redirect_to '/'
-		end 
-	end
-	
-	def destroy
-		reset_session
-		session[:user_id] = nil
-		redirect_to '/'
-	end
-	
-	private
-	def login_params
-			params.require(:login).permit(:email, :password)
-	end
-end
-
